fc4f407130748a378a667adbd1a7bba3313b39cc
kaylanhua <kaylahuang@college.harvard.edu>
2023-07-26 19:25:55 -0700

    sidebar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,44 @@
-const el = document.getElementById('nav-time');
+function setTime(){
 
-function updateClock() {
-   var now = new Date();
-   var time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds(); 
-        
-   el.innerHTML = time;    
+var text2 = document.getElementById("time");
+var now = new Date();
+var hours = now.getHours();
+var minutes = now.getMinutes();
+var seconds = now.getSeconds();
+var dn = "pm";
+if (hours<12)
+   dn="am";
+if (minutes<=9)
+   minutes="0"+minutes;
+if (seconds<=9)
+   seconds="0"+seconds;
+
+   text2.innerHTML = hours + ':' + 
+   minutes + ':' + seconds+ "<bn> ";
 }
 
-setInterval(updateClock, 1);
+setTime();
+
+setInterval(setTime, 1000);
+
+document.querySelector('.menu-btn').addEventListener('click', function() {
+var menuContent = document.querySelector('.menu-content');
+if (menuContent.style.display === 'none' || menuContent.style.display === '') {
+   menuContent.style.display = 'block';
+} else {
+   menuContent.style.display = 'none';
+}
+});
+
+document.querySelector('.menu-btn').addEventListener('click', function() {
+var menu = document.querySelector('.menu');
+menu.classList.toggle('toggled');
+});
+
+// window.onload = function() {
+//    fetch('menu.html')
+//      .then(response => response.text())
+//      .then(data => {
+//        document.getElementById('menu-container').innerHTML = data;
+//      });
+//  };
